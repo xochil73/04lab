df2829a9a9c2938b84fcf66015d1b1ccb9882075
Xochil Squaglia <xochilsquaglia@yahoo.com>
2019-02-17 09:18:36 -0800

    adding a .gitignore

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict'
 
+
+const bufferArray = ['58,6F,63,68,69,6C', '4A,6F,73,65,70,68', '4C,75,63,69,97,6E,61'];
+const testBuffer =
+
+
 const fs = require('fs');
 fs.writeFile("/tmp/test", "Hey there!", function(err) {
     if(err) {
